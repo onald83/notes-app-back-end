d95dc3422c9e4933a47cee237edeeedd1a4f3223
onald83 <onald83@gmail.com>
2022-08-26 16:05:50 +0700

    First commit

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+// const { options } = require('@hapi/hapi/lib/cors')
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler')
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    // handler: () => {}
+    handler: addNoteHandler
+    /* options: {
+        cors: {
+            origin: ['*']
+        }
+    } */
+  },
+
+  {
+    method: 'GET',
+    path: '/notes',
+    // handler: () => {}
+    handler: getAllNotesHandler
+  },
+
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    // handler: () => {}
+    handler: getNoteByIdHandler
+  },
+
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    // handler: () => {}
+    handler: editNoteByIdHandler
+  },
+
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    // handler: () => {}
+    handler: deleteNoteByIdHandler
+  }
+
+]
+
+module.exports = routes
